0354d1390324e39ac14fff5aa11c0fe1fa3dafda
Vance <slirb007@gmail.com>
2016-03-22 17:11:43 -0400

    Continued runtime error fixing

--- BoydGamingCharterSystem/Models/CharterCarrier.vb
+++ BoydGamingCharterSystem/Models/CharterCarrier.vb
@@ -1,13 +1,60 @@
 ﻿Imports System.ComponentModel.DataAnnotations
-
-
+Imports System.ComponentModel.DataAnnotations.Schema
+Imports BoydGamingCharterSystem
+<Table("Carrier")>
 Public Class CharterCarrier
-    <Key>
+
+
+
     Private carrierId As Integer
-    Private carrierName As String
+    Private carrierCompany As CharterCompany
+
+    Private carrierLicenseNumber As String
+    Private carrierInsuranceNumber As String
+    Private carrierInsuranceExpirationDate As DateTime
+
+    'Possible TODO if necessary
+    'Take this field and make it a list - a comments table will handle
+    'the comments for carriers and operators together, and data about
+    'those comments can be tracked
 
 
 
+
+    Private carrierCreateDateTime As DateTime
+
+    Private carrierLastUpdatedDateTime As DateTime
+
+    Private carrierCommentable As Commentable
+
+    'Private commentableId As Integer
+
+    '<ForeignKey("commentableId")>
+    Public Property Commentable As Commentable
+        Get
+            Return carrierCommentable
+        End Get
+        Set(value As Commentable)
+            carrierCommentable = value
+        End Set
+    End Property
+
+    <NotMapped>
+    Public Property Comments() As ICollection(Of CharterComment)
+        Get
+            Return Commentable.Comments
+        End Get
+        Set(value As ICollection(Of CharterComment))
+            Commentable.Comments = value
+        End Set
+    End Property
+
+
+    <ForeignKey("CarrierId")>
+    Public Property CharterAgreements As ICollection(Of CharterAgreement)
+
+
+    <Key>
     Public Property Id() As Integer
         Get
             Return carrierId
@@ -18,24 +65,26 @@ Public Class CharterCarrier
     End Property
 
 
-    Public Property Name() As String
+
+
+    Public Property Company As CharterCompany
         Get
-            Return carrierName
+            Return carrierCompany
         End Get
-        Set(value As String)
-            carrierName = value
+        Set(value As CharterCompany)
+            carrierCompany = value
         End Set
-
     End Property
+
     Public Sub New()
+
         Me.Id = Nothing
-        Me.Name = Nothing
+
+        Me.Commentable = New Commentable
+
     End Sub
 
-    Public Sub New(id As Integer, name As String)
-        Me.Id = id
-        Me.Name = name
-    End Sub
 
 
 End Class
+
